fb9264a4b6c10bd9d0e00a5f0086a06ba412f7de
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-07-08 22:10:58 -0700

    Fixed build

--- src/c_bindings.rs
+++ src/c_bindings.rs
@@ -3,11 +3,3 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-impl Copy for gnss_signal_t {}
-
-impl Clone for gnss_signal_t {
-    fn clone(&self) -> Self {
-        *self
-    }
-}
