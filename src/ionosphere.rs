1a28b7790ee02e139deee4b2771565fa153f6420
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-07-25 00:09:48 -0700

    Added tests for iono and tropo

--- src/ionosphere.rs
+++ src/ionosphere.rs
@@ -84,3 +84,160 @@ impl Ionosphere {
         unsafe { c_bindings::calc_ionosphere(t.c_ptr(), lat_u, lon_u, a, e, &self.0) }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{ionosphere::Ionosphere, time::GpsTime};
+
+    const D2R: f64 = std::f64::consts::PI / 180.0;
+
+    #[test]
+    fn calc_ionosphere() {
+        let t = GpsTime::new(1875, 479820.0).unwrap();
+        let i = Ionosphere::new(
+            t, 0.1583e-7, -0.7451e-8, -0.5960e-7, 0.1192e-6, 0.1290e6, -0.2130e6, 0.6554e5,
+            0.3277e6,
+        );
+        let lat_u = -35.3 * D2R;
+        let lon_u = 149.1 * D2R;
+        let a = 0.0 * D2R;
+        let e = 15.0 * D2R;
+        let d_true = 7.202;
+
+        let d_tol = 1e-3;
+
+        let d_l1 = i.calc_delay(&t, lat_u, lon_u, a, e);
+        let d_err = (d_l1 - d_true).abs();
+
+        assert!(
+            d_err < d_tol,
+            "Distance didn't match hardcoded correct value {}. Saw: {}",
+            d_true,
+            d_l1
+        );
+
+        let t = GpsTime::new(1042, 593100.).unwrap();
+        let i = Ionosphere::new(
+            t, 0.3820e-7, 0.1490e-7, -0.1790e-6, 0.0, 0.1430e6, 0.0, -0.3280e6, 0.1130e6,
+        );
+        let lat_u = 40.0 * D2R;
+        let lon_u = 260.0 * D2R;
+        let a = 210.0 * D2R;
+        let e = 20.0 * D2R;
+        let d_true = 23.784;
+
+        let d_l1 = i.calc_delay(&t, lat_u, lon_u, a, e);
+        let d_err = (d_l1 - d_true).abs();
+
+        assert!(
+            d_err < d_tol,
+            "Distance didn't match hardcoded correct values {}. Saw: {}",
+            d_true,
+            d_l1
+        );
+
+        let t = GpsTime::new(1042, 345600.).unwrap();
+        let i = Ionosphere::new(
+            t, 1.304e-8, 0., -5.96e-8, 5.96e-8, 1.106e5, -65540.0, -2.621e5, 3.932e5,
+        );
+        let lat_u = 0.70605;
+        let lon_u = -0.076233;
+        let a = 2.62049;
+        let e = 0.2939;
+        let d_true = 3.4929;
+
+        let d_l1 = i.calc_delay(&t, lat_u, lon_u, a, e);
+        let d_err = (d_l1 - d_true).abs();
+
+        assert!(
+            d_err < d_tol,
+            "Distance didn't match hardcoded correct values {}. Saw: {}",
+            d_true,
+            d_l1
+        );
+    }
+
+    #[test]
+    fn test_decode_iono_parameters() {
+        const TOL: f64 = 1e-12;
+        // struct {
+        // u32 frame_words[8];
+        // ionosphere_t result;
+        // } t_case = {.frame_words =
+        let frame_words: [u32; 8] = [
+            /* 4th SF real data at 11-May-2016 */
+            0x1e0300c9, 0x7fff8c24, 0x23fbdc2, 0, 0, 0, 0, 0,
+        ];
+        let result = Ionosphere::new(
+            /* reference data provided by u-blox receiver */
+            GpsTime::new_unchecked(0, 0.),
+            0.0000000111758,
+            0.0000000223517,
+            -0.0000000596046,
+            -0.0000001192092,
+            98304.0,
+            131072.0,
+            -131072.0,
+            -589824.0,
+        );
+
+        let i = Ionosphere::decode_parameters(&frame_words).unwrap();
+
+        assert!(
+            (i.0.a0 - result.0.a0).abs() < TOL,
+            "alfa 0 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.a0,
+            result.0.a0,
+            TOL
+        );
+        assert!(
+            (i.0.a1 - result.0.a1).abs() < TOL,
+            "alfa 1 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.a1,
+            result.0.a1,
+            TOL
+        );
+        assert!(
+            (i.0.a2 - result.0.a2).abs() < TOL,
+            "alfa 2 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.a2,
+            result.0.a2,
+            TOL
+        );
+        assert!(
+            (i.0.a3 - result.0.a3).abs() < TOL,
+            "alfa 3 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.a3,
+            result.0.a3,
+            TOL
+        );
+        assert!(
+            (i.0.b0 - result.0.b0).abs() < TOL,
+            "beta 0 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.b0,
+            result.0.b0,
+            TOL
+        );
+        assert!(
+            (i.0.b1 - result.0.b1).abs() < TOL,
+            "beta 1 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.b1,
+            result.0.b1,
+            TOL,
+        );
+        assert!(
+            (i.0.b2 - result.0.b2).abs() < TOL,
+            "beta 2 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.b2,
+            result.0.b2,
+            TOL,
+        );
+        assert!(
+            (i.0.b3 - result.0.b3).abs() < TOL,
+            "beta 3 == {:30.20}, expected {:30.20}, tolerance = {:30.20}",
+            i.0.b3,
+            result.0.b3,
+            TOL,
+        );
+    }
+}
--- src/troposphere.rs
+++ src/troposphere.rs
@@ -17,3 +17,113 @@ use crate::{c_bindings, time::GpsTime};
 pub fn calc_delay(t: &GpsTime, lat: f64, h: f64, el: f64) -> f64 {
     unsafe { c_bindings::calc_troposphere(t.c_ptr(), lat, h, el) }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        time::{GpsTime, DAY},
+        troposphere::calc_delay,
+    };
+
+    const D2R: f64 = std::f64::consts::PI / 180.0;
+
+    #[test]
+    fn calc_troposphere() {
+        const d_tol: f64 = 1e-4;
+
+        /* some tests against "true" values computed with UNB3M.f */
+        /* http://www2.unb.ca/gge/Personnel/Santos/UNB_pack.pdf */
+
+        let lat = 40.0 * D2R;
+        let h = 1300.0;
+        let doy = 32.5;
+        let el = 45.0 * D2R;
+        let d_true = 2.8567;
+
+        /* GPS week 1669 starts on 1.1.2012, so easier to generate given doy */
+        let t = GpsTime::new(1669, 0.).unwrap();
+        let t = t + DAY.mul_f64(doy);
+
+        let d_tropo = calc_delay(&t, lat, h, el);
+
+        assert!(
+            (d_tropo - d_true).abs() < d_tol,
+            "Distance didn't match hardcoded correct values {:.5}. Saw: {:.5}",
+            d_true,
+            d_tropo
+        );
+
+        let lat = -10. * D2R;
+        let h = 0.0;
+        let doy = 180.5;
+        let el = 20. * D2R;
+        let d_true = 7.4942;
+
+        let t = GpsTime::new(1669, 0.).unwrap();
+        let t = t + DAY.mul_f64(doy);
+
+        let d_tropo = calc_delay(&t, lat, h, el);
+
+        assert!(
+            (d_tropo - d_true).abs() < d_tol,
+            "Distance didn't match hardcoded correct values {:.5}. Saw: {:.5}",
+            d_true,
+            d_tropo
+        );
+
+        let lat = 75. * D2R;
+        let h = 0.0;
+        let doy = 50.5;
+        let el = 10. * D2R;
+        let d_true = 12.9004;
+
+        let t = GpsTime::new(1669, 0.).unwrap();
+        let t = t + DAY.mul_f64(doy);
+
+        let d_tropo = calc_delay(&t, lat, h, el);
+
+        assert!(
+            (d_tropo - d_true).abs() < d_tol,
+            "Distance didn't match hardcoded correct values {:.5}. Saw: {:.5}",
+            d_true,
+            d_tropo
+        );
+
+        /* altitude sanity tests */
+        let max_tropo_correction = 30.0;
+        let h = -5000.;
+        let d_tropo = calc_delay(&t, lat, h, el);
+
+        assert!(
+            d_tropo.abs() < max_tropo_correction,
+            "Sanity test fail at altitude {:.5}. : Correction was {:.5}",
+            h,
+            d_tropo
+        );
+
+        let h = 12000.;
+        let d_tropo = calc_delay(&t, lat, h, el);
+
+        assert!(
+            d_tropo.abs() < max_tropo_correction,
+            "Sanity test fail at altitude {:.5}. : Correction was {:.5}",
+            h,
+            d_tropo
+        );
+
+        /* satellite elevation sanity tests */
+        let h = 100.;
+        let elevation_testcases: [f64; 6] = [1e-3, 1e-4, 1e-5, 0., -1e3, -0.1];
+        let max_tropo_correction = 100.0;
+
+        for el in elevation_testcases.iter() {
+            let d_tropo = calc_delay(&t, lat, h, *el);
+            assert!(
+                d_tropo.abs() < max_tropo_correction,
+                "Sanity test fail at satellite elevation {:.5}. : Correction was {:.5}",
+                el,
+                d_tropo
+            );
+        }
+    }
+}
