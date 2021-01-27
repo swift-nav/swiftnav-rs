8008a807ccafba094c951503000c23ccabde8c6e
Joseph Angelo <joseph.angelo@swift-nav.com>
2021-01-26 22:40:59 -0800

    Add doc comments to everything

--- src/navmeas.rs
+++ src/navmeas.rs
@@ -1,3 +1,14 @@
+//! Raw GNSS measurement representation
+//!
+//! Raw measurements of GNSS signals have several aspects to them, from the time
+//! of flight of the signal (a.k.a. the pseudorange) to the relative velocity of
+//! the satellite (a.k.a. doppler) and the signal quality (a.k.a. CN0). The
+//! [`NavigationMeasurement`] struct stores all the needed components of a
+//! single signal measurement. Several measurements from the same point in time
+//! can be used in conjunction with [satellite ephemeris](crate::ephemeris::Ephemeris)
+//! and the [PVT solver function](crate::solver::calc_pvt) to get a position,
+//! velocity and time estimate.
+
 use crate::{c_bindings, ephemeris::SatelliteState, signal::GnssSignal};
 use std::time::Duration;
 
@@ -5,33 +16,46 @@ const NAV_MEAS_FLAG_CODE_VALID: u16 = 1 << 0;
 const NAV_MEAS_FLAG_MEAS_DOPPLER_VALID: u16 = 1 << 2;
 const NAV_MEAS_FLAG_CN0_VALID: u16 = 1 << 5;
 
+/// Represents a single raw GNSS measurement
 #[derive(Clone)]
 #[repr(transparent)]
 pub struct NavigationMeasurement(c_bindings::navigation_measurement_t);
 
 impl NavigationMeasurement {
+    /// Makes a navigation measurement with all fields invalidated
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Sets the pseudorange measurement value and marks it as valid
+    ///
+    /// Units of meters, time of flight multiplied by speed of light
     pub fn set_pseudorange(&mut self, value: f64) {
         self.0.pseudorange = value;
         self.0.flags |= NAV_MEAS_FLAG_CODE_VALID;
     }
 
+    /// Marks the pseudorange measurement as invalid
     pub fn invalidate_pseudorange(&mut self) {
         self.0.flags &= !NAV_MEAS_FLAG_CODE_VALID;
     }
 
+    /// Sets the measured doppler and marks it as valid
+    ///
+    /// Units of Hertz
     pub fn set_measured_doppler(&mut self, value: f64) {
         self.0.measured_doppler = value;
         self.0.flags |= NAV_MEAS_FLAG_MEAS_DOPPLER_VALID;
     }
 
+    /// Marks the measured doppler measurement as invalid
     pub fn invalidate_measured_doppler(&mut self) {
         self.0.flags &= !NAV_MEAS_FLAG_MEAS_DOPPLER_VALID;
     }
 
+    /// Sets the state of the satellite from which the signal originated
+    ///
+    /// The satellite state is obtained by evaluating the satellite [ephemeris](crate::ephemeris::Ephemeris::calc_satellite_state) at the time of reception of the signal
     pub fn set_satellite_state(&mut self, sat_state: &SatelliteState) {
         self.0.sat_pos = *sat_state.pos.as_array_ref();
         self.0.sat_vel = *sat_state.vel.as_array_ref();
@@ -40,27 +64,37 @@ impl NavigationMeasurement {
         self.0.sat_clock_err_rate = sat_state.clock_rate_err;
     }
 
+    /// Sets the signal CN0 measurement and marks it as valid
+    ///
+    /// Units of dB-Hz
     pub fn set_cn0(&mut self, value: f64) {
         self.0.cn0 = value;
         self.0.flags |= NAV_MEAS_FLAG_CN0_VALID;
     }
 
+    /// Marks the CN0 measurement as invalid
     pub fn invalidate_cn0(&mut self) {
         self.0.flags &= !NAV_MEAS_FLAG_CN0_VALID;
     }
 
+    /// Sets the time the signal has been continuously tracked
+    ///
+    /// Sometimes referred to as the PLL lock time
     pub fn set_lock_time(&mut self, value: Duration) {
         self.0.lock_time = value.as_secs_f64();
     }
 
+    /// Sets the signal ID of the measured signal
     pub fn set_sid(&mut self, value: GnssSignal) {
         self.0.sid = value.to_gnss_signal_t();
     }
 
-    pub fn flags_are_valid(&self) -> bool {
+    /// Checks to see if all of the measurement flags marked as valid
+    pub fn flags_are_all_valid(&self) -> bool {
         unsafe { c_bindings::nav_meas_flags_valid(self.0.flags) }
     }
 
+    /// Checks to see if the pseudorange measurement is marked as valid
     pub fn pseudorange_is_valid(&self) -> bool {
         unsafe { c_bindings::pseudorange_valid(&self.0) }
     }
@@ -72,10 +106,20 @@ impl Default for NavigationMeasurement {
     }
 }
 
+/// Encodes a [`Duration`] as an SBP lock time
+///
+/// Note: It is encoded according to DF402 from the RTCM 10403.2 Amendment 2
+/// specification.  Valid values range from 0 to 15 and the most significant
+/// nibble is reserved for future use.
 pub fn encode_lock_time(nav_meas_lock_time: Duration) -> u8 {
     unsafe { c_bindings::encode_lock_time(nav_meas_lock_time.as_secs_f64()) }
 }
 
+/// Decodes an SBP lock time value into a [`Duration`]
+///
+/// Note: It is encoded according to DF402 from the RTCM 10403.2 Amendment 2
+/// specification.  Valid values range from 0 to 15 and the most significant
+/// nibble is reserved for future use.
 pub fn decode_lock_time(sbp_lock_time: u8) -> Duration {
     let value = unsafe { c_bindings::decode_lock_time(sbp_lock_time) };
     Duration::from_secs_f64(value)
--- src/solver.rs
+++ src/solver.rs
@@ -1,3 +1,9 @@
+//! Single epoch PVT solver
+//!
+//! Several [raw measurements](crate::navmeas::NavigationMeasurement) from the
+//! same point in time can be processed to get an estimated PVT (position,
+//! velocity, and time) solution.
+
 use crate::c_bindings;
 use crate::coords::{LLHRadians, ECEF, NED};
 use crate::navmeas::NavigationMeasurement;
@@ -7,18 +13,22 @@ use std::borrow::Cow;
 use std::ffi;
 use std::fmt;
 
+/// A position velocity and time solution
 #[derive(Clone, Debug)]
 pub struct GnssSolution(c_bindings::gnss_solution);
 
 impl GnssSolution {
+    /// Checks to see if the position solution is valid
     pub fn pos_valid(&self) -> bool {
         self.0.valid == 1
     }
 
+    /// Checks to see if the velocity solution is valid
     pub fn vel_valid(&self) -> bool {
         self.0.velocity_valid == 1
     }
 
+    /// Gets the received position in latitude, longitude, and height coordinates
     pub fn pos_llh(&self) -> Option<LLHRadians> {
         if self.pos_valid() {
             Some(LLHRadians::from_array(&self.0.pos_llh))
@@ -27,6 +37,7 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the received position in earth centered earth fixed cartesian coordinates
     pub fn pos_ecef(&self) -> Option<ECEF> {
         if self.pos_valid() {
             Some(ECEF::from_array(&self.0.pos_ecef))
@@ -35,6 +46,7 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the receiver velocity in local north east down coordinates
     pub fn vel_ned(&self) -> Option<NED> {
         if self.vel_valid() {
             Some(NED::from_array(&self.0.vel_ned))
@@ -43,6 +55,7 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the receiver velocity in earth centered earth fixed cartesian coordinates
     pub fn vel_ecef(&self) -> Option<ECEF> {
         if self.vel_valid() {
             Some(ECEF::from_array(&self.0.vel_ecef))
@@ -51,6 +64,13 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the receiver position covariance matrix
+    ///
+    /// This is the row-first upper diagonal matrix of error covariances
+    /// in x, y, z (all receiver clock covariance terms are ignored).
+    ///
+    /// Index 6 is the GDOP.
+    ///
     pub fn err_cov(&self) -> Option<&[f64; 7]> {
         if self.pos_valid() {
             Some(&self.0.err_cov)
@@ -59,6 +79,9 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the receiver velocity covariance matrix
+    ///
+    /// See [`GnssSolution::err_cov`] for representation, minus the DOP element
     pub fn vel_cov(&self) -> Option<&[f64; 7]> {
         if self.vel_valid() {
             Some(&self.0.vel_cov)
@@ -67,30 +90,37 @@ impl GnssSolution {
         }
     }
 
+    /// Gets the receiver clock offset
     pub fn clock_offset(&self) -> f64 {
         self.0.clock_offset
     }
 
+    /// Gets the receiver clock offset variance
     pub fn clock_offset_var(&self) -> f64 {
         self.0.clock_offset_var
     }
 
+    /// Gets the receiver clock drift
     pub fn clock_drift(&self) -> f64 {
         self.0.clock_drift
     }
 
+    /// Gets the receiver clock drift variance
     pub fn clock_drift_var(&self) -> f64 {
         self.0.clock_drift_var
     }
 
+    /// Gets the corrected time of the measurement
     pub fn time(&self) -> GpsTime {
         GpsTime::new(self.0.time.wn, self.0.time.tow).unwrap()
     }
 
+    /// Gets the number of satellites used in the solution
     pub fn sats_used(&self) -> u8 {
         self.0.n_sats_used
     }
 
+    /// Gets the number of signals used in the solution
     pub fn signals_used(&self) -> u8 {
         self.0.n_sigs_used
     }
@@ -102,22 +132,31 @@ impl Default for GnssSolution {
     }
 }
 
+/// Dilution of precision (DOP) of a solution
+///
+/// DOP is a measurement of how the satellite geometry impacts the precision of
+/// the solution
 #[derive(Clone, Debug)]
 pub struct Dops(c_bindings::dops_t);
 
 impl Dops {
+    /// Gets the position (3D) dilution of precision
     pub fn pdop(&self) -> f64 {
         self.0.pdop
     }
+    /// Gets the geometric dilution of precision
     pub fn gdop(&self) -> f64 {
         self.0.gdop
     }
+    /// Gets the time dilution of precision
     pub fn tdop(&self) -> f64 {
         self.0.tdop
     }
+    /// Gets the horizontal dilution of precision
     pub fn hdop(&self) -> f64 {
         self.0.hdop
     }
+    /// Gets the vertical dilution of precision
     pub fn vdop(&self) -> f64 {
         self.0.vdop
     }
@@ -129,6 +168,7 @@ impl Default for Dops {
     }
 }
 
+/// Different strategies of how to choose which measurements to use in a solution
 #[derive(Copy, Clone, Debug)]
 #[repr(u32)]
 pub enum ProcessingStrategy {
@@ -138,6 +178,7 @@ pub enum ProcessingStrategy {
     L1Only = c_bindings::processing_strategy_t_L1_ONLY,
 }
 
+/// Holds the settings to customize how the GNSS solution is calculated
 #[derive(Copy, Clone, Debug)]
 pub struct PvtSettings {
     strategy: ProcessingStrategy,
@@ -146,6 +187,7 @@ pub struct PvtSettings {
 }
 
 impl PvtSettings {
+    /// Sets the processing strategy to use
     pub fn set_strategy(self, strategy: ProcessingStrategy) -> PvtSettings {
         PvtSettings {
             strategy,
@@ -154,6 +196,11 @@ impl PvtSettings {
         }
     }
 
+    /// Enables use of RAIM (receiver autonomous integrity monitoring)
+    ///
+    /// RAIM is an algorithm to detect and remove invalid measurements. Enabling
+    /// RAIM means additional computations must take place to ensure the validity
+    /// of the solution
     pub fn enable_raim(self) -> PvtSettings {
         PvtSettings {
             strategy: self.strategy,
@@ -162,6 +209,9 @@ impl PvtSettings {
         }
     }
 
+    /// Disables use of RAIM
+    ///
+    /// See [`PvtSettings::enable_raim()`] for more details
     pub fn disable_raim(self) -> PvtSettings {
         PvtSettings {
             strategy: self.strategy,
@@ -170,6 +220,9 @@ impl PvtSettings {
         }
     }
 
+    /// Enables calculation of a velocity solution
+    ///
+    /// Note: this requires the presence of doppler measurements
     pub fn enable_velocity(self) -> PvtSettings {
         PvtSettings {
             strategy: self.strategy,
@@ -178,6 +231,7 @@ impl PvtSettings {
         }
     }
 
+    /// Disables calculation of a velocity solution
     pub fn disable_velocity(self) -> PvtSettings {
         PvtSettings {
             strategy: self.strategy,
@@ -197,10 +251,12 @@ impl Default for PvtSettings {
     }
 }
 
+/// Set of signals used in calculating a GNSS solution
 #[derive(Clone)]
 pub struct SidSet(c_bindings::gnss_sid_set_t);
 
 impl SidSet {
+    /// Makes an empty set
     pub fn new() -> SidSet {
         unsafe {
             let mut ss = std::mem::zeroed::<SidSet>();
@@ -209,28 +265,39 @@ impl SidSet {
         }
     }
 
+    /// Gets the number of satellites in the set
     pub fn get_sat_count(&self) -> u32 {
         unsafe { c_bindings::sid_set_get_sat_count(&self.0) }
     }
 
+    /// Gets the number of signals in the set
     pub fn get_sig_count(&self) -> u32 {
         unsafe { c_bindings::sid_set_get_sig_count(&self.0) }
     }
 
+    /// Checks to see if a signal is present within the set
     pub fn contains(&self, sid: GnssSignal) -> bool {
         unsafe { c_bindings::sid_set_contains(&self.0, sid.to_gnss_signal_t()) }
     }
 }
 
+/// Causes of a failed PVT solution
 #[derive(Debug, Copy, Clone)]
 #[repr(usize)]
 pub enum PvtError {
+    /// The PDOP of the solution was unacceptably high
     HighPdop = 0,
+    /// Alitutde of the solution was unacceptable
     UnreasonableAltitude,
+    /// The velocity of the solution was >= 1000 kts
     HighVelocity,
+    /// RAIM check and repair was unsuccessful
     RaimRepairFailed,
+    /// RAIM check and repair was impossible due to not enough measurements
     RaimRepairImpossible,
+    /// The least squares iteration failed to converge
     FailedToConverge,
+    /// There were not enough measurements for a solution
     NotEnoughMeasurements,
 }
 
@@ -264,6 +331,7 @@ impl fmt::Display for PvtError {
 
 impl std::error::Error for PvtError {}
 
+/// Try to calculate a single point GNSS solution
 pub fn calc_pvt(
     measurements: &[NavigationMeasurement],
     tor: GpsTime,
