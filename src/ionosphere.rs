8d66e39ecadcf897979967b8c9f956b2b925e68d
Joseph Angelo <joseph.angelo@swift-nav.com>
2021-02-01 23:23:29 -0800

    Add ionosphere decode error

--- src/ionosphere.rs
+++ src/ionosphere.rs
@@ -9,10 +9,24 @@
 //!  * IS-GPS-200H, Section 20.3.3.5.2.5 and Figure 20-4
 
 use crate::{c_bindings, time::GpsTime};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
 /// Represents an ionosphere model
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct Ionosphere(c_bindings::ionosphere_t);
+
+/// An error indicating that the iono model failed to be decoded
+#[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub struct IonoDecodeFailure;
+
+impl Display for IonoDecodeFailure {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Error decoding iono model")
+    }
+}
+
+impl Error for IonoDecodeFailure {}
 
 impl Ionosphere {
     /// Construct an ionosphere model from already decoded parameters
@@ -49,7 +63,7 @@ impl Ionosphere {
     /// --------
     /// References:
     /// * IS-GPS-200H, Section 20.3.3.5.1.7
-    pub fn decode_parameters(words: &[u32; 8]) -> Option<Ionosphere> {
+    pub fn decode_parameters(words: &[u32; 8]) -> Result<Ionosphere, IonoDecodeFailure> {
         let mut iono = Ionosphere(c_bindings::ionosphere_t {
             toa: GpsTime::unknown(),
             a0: 0.0,
@@ -65,9 +79,9 @@ impl Ionosphere {
         let success = unsafe { c_bindings::decode_iono_parameters(words, &mut iono.0) };
 
         if success {
-            Some(iono)
+            Ok(iono)
         } else {
-            None
+            Err(IonoDecodeFailure)
         }
     }
 
