36336b92ab726c3559f90838ebfdfa6bd5417f33
Jason Mobarak <jason@swiftnav.com>
2021-05-28 22:34:17 -0700

    experimental wasi support

---
+++ wasi-env.sh
@@ -0,0 +1,7 @@
+export CMAKE_C_COMPILER=/opt/wasi-sdk/bin/clang
+export CMAKE_CXX_COMPILER=/opt/wasi-sdk/bin/clang++
+export CMAKE_SHARED_LIBRARY_LINK_C_FLAGS=""
+export CMAKE_EXE_LINKER_FLAGS=""
+export CMAKE_AR=/opt/wasi-sdk/bin/llvm-ar
+export CMAKE_LINKER=/opt/wasi-sdk/bin/wasm-ld
+export CMAKE_RANLIB=/opt/wasi-sdk/bin/llvm-ranlib
