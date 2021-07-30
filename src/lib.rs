52537f7a94cdfde2812d90902728b14c14cb6922
Joseph Angelo <joseph.angelo@swift-nav.com>
2021-07-29 21:42:44 -0700

    Added a licence comment to each source file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+// Copyright (c) 2020-2021 Swift Navigation Inc.
+// Contact: Swift Navigation <dev@swiftnav.com>
+//
+// This source is subject to the license found in the file 'LICENSE' which must
+// be be distributed together with this source. All other rights reserved.
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 //! `swiftnav-rs` is a library that implements GNSS utility functions to perform
 //! position estimations. The data used by `swiftnav-rs` typically comes from GNSS
 //! receiver chips as raw observation and ephemeris data. `swiftnav-rs` is more of
