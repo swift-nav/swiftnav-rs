7d49308f6956a6798cb1b57c140d235e06bb939d
Joseph Angelo <joseph.angelo@swift-nav.com>
2021-11-29 18:53:53 -0800

    Added a doc comment for the geoid module (#79)

--- swiftnav/src/geoid.rs
+++ swiftnav/src/geoid.rs
@@ -1,3 +1,22 @@
+// Copyright (c) 2020-2021 Swift Navigation Inc.
+// Contact: Swift Navigation <dev@swiftnav.com>
+//
+// This source is subject to the license found in the file 'LICENSE' which must
+// be be distributed together with this source. All other rights reserved.
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//! Geoid height calculation
+//!
+//! GNSS positions have a height which is usually referenced to a ellipsoid,
+//! a smooth surface which very roughly approximates the mean sea level. This
+//! ellipsoid can deviate by a large amount for the actual local mean sea level
+//! due to the variations in the local gravitational field. A geoid model is
+//! built to better approximate these variations in mean sea level, and can be
+//! used to give a height relative to mean sea level which can be more helpful
+//! to an end user.
+
 use crate::coords::LLHRadians;
 
 /// List of potential Geoid models used
